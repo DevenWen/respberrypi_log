71eb794242b1c73a12f01b5aecba5e11fe9ad524
DavenWen.pi <kangqiang.w@gmail.com>
2021-10-21 01:08:57 +0800

    learn: opencv mediapipe

---
+++ opencv/mediapipe/hand_tracking_min.py
@@ -0,0 +1,19 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
