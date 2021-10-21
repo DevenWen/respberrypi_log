faf9e3a1ce7488da545bb929e794db2e35e061ca
DavenWen.pi <kangqiang.w@gmail.com>
2021-10-21 22:32:06 +0800

    update tracking min

--- opencv/mediapipe/hand_tracking_min.py
+++ opencv/mediapipe/hand_tracking_min.py
@@ -7,13 +7,25 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+pTime = 0
+cTime = 0
+
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id,lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
